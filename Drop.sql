6dc4316bd9d85f0e7c4c97cc14681954e2dc8111
yangyang01011001 <yangyang01011001@gmail.com>
2015-11-06 15:12:55 -0600

    Create Drop.sql

---
+++ Drop.sql
@@ -0,0 +1,24 @@
+set feedback on
+prompt dropping tables ...
+set feedback off
+
+
+
+drop table teams cascade constraints purge;
+drop table players cascade constraints purge;
+drop table officials cascade constraints purge;
+drop table hotels cascade constraints purge;
+drop table fans cascade constraints purge;
+drop table stadiums cascade constraints purge;
+drop table matches cascade constraints purge;
+drop table favStadiums cascade constraints purge;
+drop table favPlayers cascade constraints purge;
+drop table favTeams cascade constraints purge;
+drop table autographs cascade constraints purge;
+drop table medicalServices cascade constraints purge;
+drop table Q2 purge;
+drop table Q3 purge;
+drop table Q2_EXPECTED purge;
+drop table Q3_EXPECTED purge;
+drop table Q4_EXPECTED purge;
+
